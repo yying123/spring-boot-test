98974d2bb366559ed7f1e9823d63ff21d516a02d
liangjiajie <466087152@qq.com>
2024-05-17 09:41:03 +0800

    修改测试

--- src/main/java/com/ljj/myspringboot/demos/web/BasicController.java
+++ src/main/java/com/ljj/myspringboot/demos/web/BasicController.java
@@ -32,7 +32,7 @@ public class BasicController {
     @RequestMapping("/hello")
     @ResponseBody
     public String hello(@RequestParam(name = "name", defaultValue = "unknown user") String name) {
-        return "Hello2 " + name;
+        return "Hello~~~~ " + name;
     }
 
     // http://127.0.0.1:8080/user
